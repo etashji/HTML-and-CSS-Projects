a78e2b52592ab9d7c90c85d18be5723ae3724b0c
etashji <99293073+etashji@users.noreply.github.com>
2022-02-25 14:39:45 -0500

    Portfolio Slideshow

--- Portfolio/JS/portfolio.js
+++ Portfolio/JS/portfolio.js
@@ -23,18 +23,22 @@ function currentSlide(n) {
 
 
 function showSlides(n) {
+    var i;
     var slides = document.getElementsByClassName("mySlides"); // This takes all elements with the class name "mySlides" and stores them in the variable array "slides"
     var dots = document.getElementsByClassName("dot"); // This takes all elements with the class name "dot" and stores them in the variable array "dots"
     if (n > slides.length) {slideIndex = 1}; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
     if (n < 1) {slideIndex = slides.length}; // If n (the number passed into the function) is less than 1, the slideIndex is set to the length of the array "slides"
-    for (i = 0; i < slides.legnth; i++) {
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"; // This for loop takes each item in the array "slides" and sets the display to none
     }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", ""); //This for loop takes each item in the array "dots" and removes "active" which removes the active styling
     }
     slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
     dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
+    setTimeout(showSlides, 5000);
 }
 
 // This code will create close the contact from when the user clicks off of it
